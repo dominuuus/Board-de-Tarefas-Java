51c0ddc5ea979d21d2dca867c9e053592bf6560f
Domingos <dmgssantiago@gmail.com>
2025-04-06 10:47:35 -0300

    Criação das exceptions

---
+++ src/main/java/com/boardtarefas/BoardTarefas/exception/CardBlockedException.java
@@ -0,0 +1,7 @@
+package com.boardtarefas.BoardTarefas.exception;
+
+public class CardBlockedException extends RuntimeException{
+    public CardBlockedException(String message) {
+        super(message);
+    }
+}
